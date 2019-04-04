11e1c2ff4d5b1fd49b94af953438a3b36c10aee3
johanqr <49080113+johanqr@users.noreply.github.com>
2019-04-03 22:10:10 -0600

    Add files via upload

---
+++ Tarea1/Tarea1.py
@@ -0,0 +1,7 @@
+import math
+num = str(math.pi)
+num
+'3.1415926'
+mostrar = '%s %.9s %s %.8s %s %.7s %s %.6s %s %.5s %s %.4s' % ('*',num,'**',num,'***',num,'****',num,'*****',num,'******',num)
+print(mostrar)
+
